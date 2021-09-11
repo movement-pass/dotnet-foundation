9694310e3193dbc68eb36d4dea20fed245515468
Kazi Manzur Rashid <kazimanzurrashid@gmail.com>
2021-09-11 22:47:34 +0600

    feat: added passes load kinesis stream

---
+++ MovementPass.Foundation.Stacks/PassesLoadStream.cs
@@ -0,0 +1,19 @@
+﻿namespace MovementPass.Foundation.Stacks
+{
+    using Amazon.CDK;
+    using Amazon.CDK.AWS.Kinesis;
+
+    public sealed class PassesLoadStream : BaseStack
+    {
+        public PassesLoadStream (
+            Construct scope,
+            string id,
+            IStackProps props = null) : base(scope, id, props) =>
+            // ReSharper disable once ObjectCreationAsStatement
+            new Stream(this, "Stream",
+                new StreamProps {
+                    StreamName = $"{this.App}_passes-load_{this.Version}",
+                    RetentionPeriod = Duration.Hours(24)
+                });
+    }
+}
