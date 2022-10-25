527534f781839956867eb797f0c7635f2094ea19
Kazi Manzur Rashid <kazimanzurrashid@gmail.com>
2022-10-26 03:49:38 +0600

    refactor: use file scoped namespace

--- MovementPass.Foundation.Stacks/PassesLoadQueue.cs
+++ MovementPass.Foundation.Stacks/PassesLoadQueue.cs
@@ -1,26 +1,25 @@
-﻿namespace MovementPass.Foundation.Stacks
+﻿namespace MovementPass.Foundation.Stacks;
+
+using Amazon.CDK;
+using Amazon.CDK.AWS.SQS;
+
+public sealed class PassesLoadQueue : BaseStack
 {
-    using Amazon.CDK;
-    using Amazon.CDK.AWS.SQS;
+    public PassesLoadQueue(
+        Construct scope,
+        string id,
+        IStackProps props = null) : base(scope, id, props) =>
 
-    public sealed class PassesLoadQueue : BaseStack
-    {
-        public PassesLoadQueue(
-            Construct scope,
-            string id,
-            IStackProps props = null) : base(scope, id, props) =>
-
-            // ReSharper disable once ObjectCreationAsStatement
-            new Queue(this, "LoadQueue",
-                new QueueProps
-                {
-                    QueueName = $"{this.App}_passes_load_{this.Version}.fifo",
-                    ReceiveMessageWaitTime = Duration.Seconds(20),
-                    VisibilityTimeout = Duration.Minutes(5),
-                    Fifo = true,
-                    ContentBasedDeduplication = true,
-                    FifoThroughputLimit = FifoThroughputLimit.PER_MESSAGE_GROUP_ID,
-                    DeduplicationScope = DeduplicationScope.MESSAGE_GROUP,
-                });
-    }
+        // ReSharper disable once ObjectCreationAsStatement
+        new Queue(this, "LoadQueue",
+            new QueueProps
+            {
+                QueueName = $"{this.App}_passes_load_{this.Version}.fifo",
+                ReceiveMessageWaitTime = Duration.Seconds(20),
+                VisibilityTimeout = Duration.Minutes(5),
+                Fifo = true,
+                ContentBasedDeduplication = true,
+                FifoThroughputLimit = FifoThroughputLimit.PER_MESSAGE_GROUP_ID,
+                DeduplicationScope = DeduplicationScope.MESSAGE_GROUP,
+            });
 }
