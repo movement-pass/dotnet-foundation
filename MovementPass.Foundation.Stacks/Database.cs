303166f7553ef6910322e302d18c9f6a5694b61a
Kazi Manzur Rashid <kazimanzurrashid@gmail.com>
2022-10-26 03:39:07 +0600

    refactor: use file scoped namespace

--- MovementPass.Foundation.Stacks/Database.cs
+++ MovementPass.Foundation.Stacks/Database.cs
@@ -1,76 +1,75 @@
-﻿namespace MovementPass.Foundation.Stacks
+﻿namespace MovementPass.Foundation.Stacks;
+
+using System;
+
+using Amazon.CDK;
+using Amazon.CDK.AWS.DynamoDB;
+
+public class Database : BaseStack
 {
-    using System;
-
-    using Amazon.CDK;
-    using Amazon.CDK.AWS.DynamoDB;
-
-    public class Database : BaseStack
+    public Database(Construct scope, string id, IStackProps props = null)
+        : base(scope, id, props)
     {
-        public Database(Construct scope, string id, IStackProps props = null)
-            : base(scope, id, props)
-        {
-            this.CreateTable(
-                "applicants",
-                CreateAttribute("id"),
-                StreamViewType.NEW_IMAGE
-            );
+        this.CreateTable(
+            "applicants",
+            CreateAttribute("id"),
+            StreamViewType.NEW_IMAGE
+        );
 
-            this.CreateTable("passes",
-                CreateAttribute("id"),
-                StreamViewType.NEW_IMAGE,
-                table =>
-                {
-                    table.AddGlobalSecondaryIndex(
-                        new GlobalSecondaryIndexProps
-                        {
-                            IndexName = "ix_applicantId-endAt",
-                            PartitionKey = CreateAttribute("applicantId"),
-                            SortKey = CreateAttribute("endAt")
-                        });
-
-                    table.AddGlobalSecondaryIndex(
-                        new GlobalSecondaryIndexProps
-                        {
-                            IndexName = "ix_applicantId-status",
-                            PartitionKey = CreateAttribute("applicantId"),
-                            SortKey = CreateAttribute("status")
-                        });
-                });
-        }
-
-        private static IAttribute CreateAttribute(string name) =>
-            new Amazon.CDK.AWS.DynamoDB.Attribute
+        this.CreateTable("passes",
+            CreateAttribute("id"),
+            StreamViewType.NEW_IMAGE,
+            table =>
             {
-                Name = name,
-                Type = AttributeType.STRING
-            };
+                table.AddGlobalSecondaryIndex(
+                    new GlobalSecondaryIndexProps
+                    {
+                        IndexName = "ix_applicantId-endAt",
+                        PartitionKey = CreateAttribute("applicantId"),
+                        SortKey = CreateAttribute("endAt")
+                    });
 
-        private void CreateTable(
-            string name,
-            IAttribute partitionKey,
-            StreamViewType stream,
-            Action<Table> configure = null)
-        {
-            var table = new Table(this, $"{name}Table", new TableProps
-            {
-                TableName = $"{this.App}_{name}_{this.Version}",
-                PartitionKey = partitionKey,
-                Stream = stream,
-                BillingMode = BillingMode.PAY_PER_REQUEST,
-                RemovalPolicy = RemovalPolicy.RETAIN,
-                PointInTimeRecovery = true
+                table.AddGlobalSecondaryIndex(
+                    new GlobalSecondaryIndexProps
+                    {
+                        IndexName = "ix_applicantId-status",
+                        PartitionKey = CreateAttribute("applicantId"),
+                        SortKey = CreateAttribute("status")
+                    });
             });
+    }
 
-            configure?.Invoke(table);
+    private static IAttribute CreateAttribute(string name) =>
+        new Amazon.CDK.AWS.DynamoDB.Attribute
+        {
+            Name = name,
+            Type = AttributeType.STRING
+        };
 
-            this.PutParameterStoreValue(
-                $"dynamodbTables/{name}",
-                table.TableName);
+    private void CreateTable(
+        string name,
+        IAttribute partitionKey,
+        StreamViewType stream,
+        Action<Table> configure = null)
+    {
+        var table = new Table(this, $"{name}Table", new TableProps
+        {
+            TableName = $"{this.App}_{name}_{this.Version}",
+            PartitionKey = partitionKey,
+            Stream = stream,
+            BillingMode = BillingMode.PAY_PER_REQUEST,
+            RemovalPolicy = RemovalPolicy.RETAIN,
+            PointInTimeRecovery = true
+        });
 
-            this.PutParameterStoreValue(
-                $"dynamodbTables/{name}StreamArn",
-                table.TableStreamArn);
-        }
+        configure?.Invoke(table);
+
+        this.PutParameterStoreValue(
+            $"dynamodbTables/{name}",
+            table.TableName);
+
+        this.PutParameterStoreValue(
+            $"dynamodbTables/{name}StreamArn",
+            table.TableStreamArn);
     }
 }
